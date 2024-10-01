1f6eb9ade17539bc6ad8eec65981692b20ced5d9
DaltonSacramento <dalton.sacramento@sou.unifal-mg.edu.br>
2024-10-01 11:12:11 -0300

    inicio

--- arvores.c
+++ arvores.c
@@ -4,9 +4,54 @@
 #include <time.h>       
 #include "arvores.h"
 
-double arvore_binaria(int instancia_num) {
+// Cria um novo nó na árvore
+struct node* novo_no(int item) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->valor = item;
+    temp->esquerda = temp->direita = NULL;
+    return temp;
+}
+ 
+// Inserir novo nó com determinado
+// valor na árvore binária
+struct node* inserir(struct node* node, int valor) {
+    // Se a árvore é vazia, então cria um novo nó
+    if (node == NULL) {
+        return novo_no(valor);
+    }
+ 
+    // Caso contrário, faz uma busca pelo local adequado de inserção
+    if (valor < node->valor) {
+        node->esquerda = inserir(node->esquerda, valor);
+    } else if (valor > node->valor) {
+        node->direita = inserir(node->direita, valor);
+    }
+    
+    // Retorna o ponteiro para o nó
+    return node;
+}
+
+double arvore_binaria(int instancia_num, FILE *pontarq) {
     double tempo = 0;
     clock_t begin = clock();
+
+    // Define a árvore como uma estrutura vazia
+    struct node* root = NULL;
+
+    char letter;
+    while(letter!='I'){
+        letter=fgetc(pontarq);
+        if(letter=='R'){
+            break;
+        }
+        fgetc(pontarq);
+        if(letter=='I'){
+            int num=fgetc(pontarq);
+            inserir(root,num);
+        }
+        fgetc(pontarq);
+    }
+    
 
     
     clock_t end = clock();
@@ -35,19 +80,26 @@ int main(int argc, char* argv[]) {
     ///////////////// Não deve ser modificado /////////////////
     ///////////////////////////////////////////////////////////
     int instancia_num = -1;
+    FILE* pontarq = fopen(argv[1], "r");
+    if (pontarq == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+
     instancia_num = atoi(argv[1]);
     if (instancia_num <= 0 || instancia_num > 10) {
         printf("Para executar o código, digite ./arvores x\nonde x é um número entre 1 e 10 que simboliza a instância utilizada\n");
         exit(0);
     }
     
-    double tempo_n_balanceada = arvore_binaria(instancia_num);
-    double tempo_balanceada = arvore_balanceada(instancia_num);
+    
+    double tempo_n_balanceada = arvore_binaria(instancia_num,pontarq);
+    //double tempo_balanceada = arvore_balanceada(instancia_num);
 
     
 
     
-    printf("%f\n%f\n", tempo_n_balanceada, tempo_balanceada);
+    printf("%f\n", tempo_n_balanceada);
 
     return (1);
 }
--- arvores.h
+++ arvores.h
@@ -3,7 +3,16 @@
 
 // Implementar qualquer struct que for necessária aqui
 
-double arvore_binaria(int instancia_num);
+struct node {
+    int valor;
+    struct node *esquerda, *direita;
+};
+
+struct node* novo_no(int item);
+struct node* inserir(struct node* node, int valor);
+struct node* buscar(struct node* root, int valor);
+
+double arvore_binaria(int instancia_num, FILE *pontarq);
 double arvore_balanceada(int instancia_num);
 
 #endif // _H_ARVORES
